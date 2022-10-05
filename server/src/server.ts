1a112ca9fbd2bb99bff9b1d6e628010f84aef7b9
aeross <aerosstube@gmail.com>
2022-10-05 20:39:54 +0300

    init models

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,23 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express from "express";
-import {application} from "../config/config";
-import {routerApp} from "./routers/init-router";
+import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import {SequelizeConnect} from "./services/database-connect";
+import cors from 'cors';
+import express from 'express';
+import {application} from '../config/config';
+import {routerApp} from './routers/init-router';
+import {SequelizeConnect} from './services/database-connect';
 
 export const app = express();
 
 export const run = async () => {
-    await SequelizeConnect.authenticate();
-    await SequelizeConnect.sync();
+	await SequelizeConnect.authenticate();
+	await SequelizeConnect.sync();
 
-    app
-        .use(cors())
-        .use(bodyParser.json())
-        .use(cookieParser())
-        .use('/api', routerApp)
-        .listen(application.port, () => {
-            console.log(`Server listening on port = ${application.port}`)
-        })
-}
+	app
+		.use(cors())
+		.use(bodyParser.json())
+		.use(cookieParser())
+		.use('/api', routerApp)
+		.listen(application.port, () => {
+			console.log(`Server listening on port = ${application.port}`);
+		});
+};
