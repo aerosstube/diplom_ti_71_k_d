7aea198dc7d06d0882f48ff5c7eecf9a6f6e2bda
aeross <aerosstube@gmail.com>
2022-10-31 20:01:53 +0300

    feat(group): Добавление разметки для сервиса group.

---
+++ server/src/admin/controllers/group.controller.ts
@@ -0,0 +1,7 @@
+import {Request, Response, NextFunction} from 'express';
+
+export class  GroupController {
+	static async createGroup(req: Request, res: Response, next: NextFunction) {
+
+	}
+}
---
+++ server/src/admin/routers/group.router.ts
@@ -0,0 +1,6 @@
+import {Router} from 'express';
+
+const groupRouter: Router = Router();
+
+groupRouter
+	.post('/create', )
