605783cfdf40b77f0ffbf5f9ad60d04a5a4b54da
polun <polundradanila@gmail.com>
2022-11-01 18:54:53 +0300

    fix(props): Изменение структуры компонента

--- client/src/UI/learningDay/learningDay.tsx
+++ client/src/UI/learningDay/learningDay.tsx
@@ -3,18 +3,24 @@ import cl from './learningDay.module.css'
 import Lesson, {LessonProps} from "../Lesson/Lesson";
 
 
-interface LearningDayProps {
-    lessons:LessonProps[] | undefined;
+export interface LearningDayProps {
+    nameOfDay: string;
+    lessons: LessonProps[] | undefined;
 }
 
-const LearningDay: FC<LearningDayProps> = ({lessons}) => {
+const LearningDay: FC<LearningDayProps> = ({lessons, nameOfDay}) => {
     return (
-        <div className={cl.learningDay}>
-            {
-                lessons && lessons.map((lesson) => <Lesson title={lesson.title} body={lesson.body} mark={lesson.mark}/>)
-            }
-        </div>
+        <div className={cl.dayPlace}>
+            <p className={cl.textDay}>{nameOfDay}</p>
+            <div className={cl.learningDay}>
+                {
+                    lessons && lessons.map((lesson) => <Lesson title={lesson.title} body={lesson.body}
+                                                               mark={lesson.mark}/>)
 
+
+                }
+            </div>
+        </div>
     );
 };
 
