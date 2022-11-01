fe0154b770af78ba3da757d2ccdc39ad99347073
aeross <aerosstube@gmail.com>
2022-11-01 23:59:59 +0300

    refactor(code): refactor code.

--- server/src/client/controllers/inviteCode.controller.ts
+++ server/src/client/controllers/inviteCode.controller.ts
@@ -1,11 +1,15 @@
 import {NextFunction, Request, Response} from 'express';
 import {application} from '../../../config/config';
+import {ApiError} from '../errors/api.error';
 import {InviteCodeBusinessService} from '../services/inviteCode-service/inviteCode.business.service';
 
 export class InviteCodeController {
 	static async inviteCodeRedirect(req: Request, res: Response, next: NextFunction) {
 		try {
-			const inviteCode: string = req.body;
+			if (req.cookies.refreshToken)
+				next(ApiError.BadRequest('Вы уже авторизованы!'));
+
+			const {inviteCode} = req.body;
 			await InviteCodeBusinessService.checkInviteCode(inviteCode);
 
 			res.redirect(`https://${application.domain}:${application.port}/hello/registration`);
--- server/src/client/services/inviteCode-service/inviteCode.business.service.ts
+++ server/src/client/services/inviteCode-service/inviteCode.business.service.ts
@@ -4,8 +4,9 @@ import {InviteCodeService} from './inviteCode.service';
 export class InviteCodeBusinessService {
 	static async checkInviteCode(inviteCode: string): Promise<void> {
 		const isInviteCodeValid = await InviteCodeService.checkInviteCode(inviteCode);
-		console.log('-=========================-========-==-=- DSDASDSA' + isInviteCodeValid);
+
 		if (!isInviteCodeValid)
 			throw ApiError.BadRequest('Код приглашения невалидный!');
 	}
+
 }
