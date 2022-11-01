37d31ca1966c04ecea71b913407b8cabe4117638
aeross <aerosstube@gmail.com>
2022-11-01 23:59:36 +0300

    feat(invitecode): Добавление функции удаления инвайт кода.

--- server/src/client/services/inviteCode-service/inviteCode.service.ts
+++ server/src/client/services/inviteCode-service/inviteCode.service.ts
@@ -1,9 +1,14 @@
-import {InveiteCodeDatabaseService} from './inveiteCode.database.service';
+import {InviteCodeDatabaseService} from './inviteCode.database.service';
 
 export class InviteCodeService {
 	static async checkInviteCode(inviteCode: string): Promise<boolean> {
-		const inviteCodeDB = await InveiteCodeDatabaseService.findInviteCode(inviteCode);
+		const inviteCodeDB = await InviteCodeDatabaseService.findInviteCode(inviteCode);
 
 		return !!inviteCodeDB;
 	}
-}
+
+	static async deleteInviteCode(inviteCode: string): Promise<number> {
+		return await InviteCodeDatabaseService.deleteInviteCode(inviteCode);
+	}
+
+} //TODO: Исправить код для удаления в регистрации
