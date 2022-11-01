e715eebee6cda8a2e57b5cca04f5384171d70e8b
polun <polundradanila@gmail.com>
2022-11-01 18:49:35 +0300

    fix(placeholder): Изменение структуры объекта

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,71 @@
 import React from 'react';
 import './App.css';
 import LearningDay from "./UI/learningDay/learningDay";
+import LearningWeek from "./UI/LearningWeek/LearningWeek";
+import LessonPage from "./UI/LessonPage/LessonPage";
+import {ReactComponent} from "*.svg";
+import Regestration from "./UI/Regestration/Regestration";
 
 function App() {
-    const lessons = [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5},
-        {title: 'qwer', body: 'dfsdfadsfadsf', mark: 5},
-        {title: 'ytrewq', body: '1231321', mark: 5},
-        {title: 'ytrewq', body: '1231321', mark: 5}
-        ]
+    const days = [
+        {
+            nameOfDay: 'понедельник',
+            lessons: [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {
+                title: 'dfafasd',
+                body: 'dfsdfadsfadsf',
+                mark: 5
+            }, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}]
+
+        },
+        {
+            nameOfDay: 'Вторник',
+            lessons: [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {
+                title: 'dfafasd',
+                body: 'dfsdfadsfadsf',
+                mark: 5
+            }, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}]
+
+        },
+        {
+            nameOfDay: 'Среда',
+            lessons: [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {
+                title: 'dfafasd',
+                body: 'dfsdfadsfadsf',
+                mark: 5
+            }, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}]
+
+        },
+        {
+            nameOfDay: 'Четверг',
+            lessons: [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {
+                title: 'dfafasd',
+                body: 'dfsdfadsfadsf',
+                mark: 5
+            }, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {
+                title: 'dfafasd',
+                body: 'dfsdfadsfadsf',
+                mark: 5
+            }]
+
+        },
+        {
+            nameOfDay: 'Пятница',
+            lessons: [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {
+                title: 'dfafasd',
+                body: 'dfsdfadsfadsf',
+                mark: 5
+            }, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}, {title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}]
+
+        },
+        {
+            nameOfDay: 'Суббота',
+            lessons: [{title: 'dfafasd', body: 'dfsdfadsfadsf', mark: 5}]
+
+        }
+    ]
     return (
         <div className="App">
-            <LearningDay lessons={lessons}/>
+            <LearningWeek days={days}/>
         </div>
     );
 }
