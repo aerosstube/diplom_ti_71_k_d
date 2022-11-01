1239ffdc02f6c6d67c2ae36267ad116229c97947
polun <polundradanila@gmail.com>
2022-11-01 18:55:52 +0300

    feat(component): Добавление компонента "учебная неделя"

---
+++ client/src/UI/LearningWeek/LearningWeek.tsx
@@ -0,0 +1,17 @@
+import {FC} from 'react';
+import LearningDay, {LearningDayProps} from "../learningDay/learningDay";
+import cl from './LearningWeek.module.css'
+export interface LearningWeekProps {
+    days: LearningDayProps[] | undefined;
+}
+const LearningWeek:FC<LearningWeekProps> = ({days}) => {
+    return (
+        <div className={cl.learningWeek}>
+            {
+                days && days.map((day)=><LearningDay nameOfDay={day.nameOfDay} lessons={day.lessons}/>)
+                
+            }
+        </div>
+    );
+};
+export default LearningWeek;
