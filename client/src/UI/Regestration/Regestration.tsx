5c0104ae8faa1d15f07294a7c2d6c7e892e01391
polun <polundradanila@gmail.com>
2022-11-02 15:06:56 +0300

    feat(component): Добавление управляемых инпутов

--- client/src/UI/Regestration/Regestration.tsx
+++ client/src/UI/Regestration/Regestration.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, TextField} from "@mui/material";
 import cl from './Regestration.module.css'
 import shp from './image-ep0zmoEb0-transformed.png'
 
 const Regestration = () => {
+    const [login, setLogin] = useState('LOGIN')
+    const [password, setPassword] = useState('PASSWORD')
     return (
         <form className={cl.formReg}>
             <img
@@ -12,15 +14,19 @@ const Regestration = () => {
             />
             <p className={cl.textReg}>Добро пожаловать на школьный портал!</p>
             <TextField
+                value={login}
                 id="outlined-name"
                 label="Log in"
                 className={cl.inpReg}
+                onChange={(e) => setLogin(e.target.value)}
             />
             <TextField
+                value={password}
                 id="outlined-name"
                 label="Password"
                 className={cl.inputReg}
                 type='password'
+                onChange={(e) => setPassword(e.target.value)}
             />
             <Button className={cl.buttonReg}>ВОЙТИ</Button>
         </form>
