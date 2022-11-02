48e7fde9be6dd23900a193a87302f1c86fe3580b
polun <polundradanila@gmail.com>
2022-11-02 15:06:17 +0300

    feat(component): Изменение структуры компонента

--- client/src/UI/LessonPage/LessonPage.tsx
+++ client/src/UI/LessonPage/LessonPage.tsx
@@ -5,24 +5,38 @@ import {ListItemButton, ListItemText} from "@mui/material";
 const LessonPage = () => {
     return (
         <div className={cl.lessonPage}>
+            <p className={cl.lessonName}>МДК 04 01 рвыдsldhjfsdlkjhfskdhjfл</p>
+            <div className={cl.lessonContainer}>
+                <div className={cl.hometasks}>
+                    <p className={cl.lessonBlocksText}>Домашнее задание</p>
+                    <ol>
+                        <li>Посмотреть видео</li>
+                        <li>Написать конспект</li>
+                        <li>Устроить проект Разгром</li>
+                    </ol>
 
-            <div className={cl.hometasks}>
+                </div>
+                <div className={cl.hometasks}>
+                    <p className={cl.lessonBlocksText}>План урока</p>
+                    <ol>
+                        <li>Проверка домашнего задания</li>
+                        <li>Изучение новой темы</li>
+                        <li>Составление конспекта</li>
+                    </ol>
 
+                </div>
+                <ListItemButton component="a" href="#simple-list">
+                    <ListItemText
+                        primary='Оценка за урок'
+                        secondary='Срез знаний'
+
+                    />
+                    <ListItemText
+                        primary='5'
+                        className={cl.mark}
+                    />
+                </ListItemButton>
             </div>
-            <div className={cl.hometasks}>
-
-            </div>
-            <ListItemButton component="a" href="#simple-list">
-                <ListItemText
-                    primary='Оценка'
-                    secondary='За что'
-
-                />
-                <ListItemText
-                    primary='5'
-                    className={cl.mark}
-                />
-            </ListItemButton>
         </div>
     );
 };
