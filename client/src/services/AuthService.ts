4add1a5bf02c6c828b1c9f52f82c123289d3e23a
polun <polundradanila@gmail.com>
2022-11-12 19:02:20 +0300

    feat(service): Форма авторизации

---
+++ client/src/services/AuthService.ts
@@ -0,0 +1,24 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
+import {IAuth, ITokens, IUserData} from '../models/IAuth';
+
+export const authAPI = createApi({
+    reducerPath: 'authAPI',
+    baseQuery: fetchBaseQuery({baseUrl: 'http://10.99.97.13:8081/api/auth'}),
+    endpoints: (build) => ({
+        userLogin: build.mutation<ITokens, IAuth>({
+            query: (user) => ({
+                url: '/login',
+                method: 'POST',
+                body: {user}
+            })
+        }),
+        userRegestration: build.mutation<ITokens,IUserData>({
+            query: (user) => ({
+                url: '/registration',
+                method: 'POST',
+                body: {user}
+            })
+
+        })
+    })
+});
