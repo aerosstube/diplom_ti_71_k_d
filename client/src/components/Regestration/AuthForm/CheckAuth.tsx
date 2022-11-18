149412f457e88c891dd28ba250768e05a4d5cce3
daniel <polundradanila@gmail.com>
2022-11-18 14:40:45 +0300

    feat(function): Добавление функции поиска начала недели

--- client/src/components/Regestration/AuthForm/CheckAuth.tsx
+++ client/src/components/Regestration/AuthForm/CheckAuth.tsx
@@ -5,8 +5,22 @@ import {useAppSelector} from "../../../hooks/hook";
 
 // @ts-ignore
 const CheckAuth = ({children}) => {
+    // @ts-ignore
+    function getMonday(d) {
+  d = new Date(d);
+  let day = d.getDay(),
+      diff = d.getDate() - day + (day == 0 ? -6:1);
+  let res = new Date(d.setDate(diff));
+  res.setHours(0);
+  res.setMinutes(0);
+  res.setSeconds(0);
+  res.setMilliseconds(0);
+  res.toISOString();
+  return res;
+}
+
+console.log(getMonday(new Date()));
     const location=useLocation();
-    console.log(location.pathname)
     const isLogged= useAppSelector(state => state.userReducer.tokens.tokens.accessToken)
     if (isLogged===''){
         return <Navigate to='/auth' state={{state:location.pathname}}/>
