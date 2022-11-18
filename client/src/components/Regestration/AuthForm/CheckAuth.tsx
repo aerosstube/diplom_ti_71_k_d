7675da04ad825f74a39b89c1e97dc0d781fb9461
daniel <polundradanila@gmail.com>
2022-11-18 16:16:03 +0300

    fix(component): Исправление структуры компонента

--- client/src/components/Regestration/AuthForm/CheckAuth.tsx
+++ client/src/components/Regestration/AuthForm/CheckAuth.tsx
@@ -5,21 +5,6 @@ import {useAppSelector} from "../../../hooks/hook";
 
 // @ts-ignore
 const CheckAuth = ({children}) => {
-    // @ts-ignore
-    function getMonday(d) {
-  d = new Date(d);
-  let day = d.getDay(),
-      diff = d.getDate() - day + (day == 0 ? -6:1);
-  let res = new Date(d.setDate(diff));
-  res.setHours(0);
-  res.setMinutes(0);
-  res.setSeconds(0);
-  res.setMilliseconds(0);
-  res.toISOString();
-  return res;
-}
-
-console.log(getMonday(new Date()));
     const location=useLocation();
     const isLogged= useAppSelector(state => state.userReducer.tokens.tokens.accessToken)
     if (isLogged===''){
