a450f9e0ab3ad4446551d1b83e6357fcbbe1fc89
aeross <aerosstube@gmail.com>
2022-11-19 11:14:26 +0300

    fix(schedule): Исправление запроса для получения распиисания

--- server/src/client/controllers/schedule.controller.ts
+++ server/src/client/controllers/schedule.controller.ts
@@ -1,11 +1,15 @@
 import { NextFunction, Response } from 'express';
+import { ApiError } from '../errors/api.error';
 import { RequestWithUser } from '../middlewares/auth-middleware';
 import { ScheduleBusinessService } from '../services/schedule-services/schedule.business.service';
 
 export class ScheduleController {
 	static async getScheduleWeek(req: RequestWithUser, res: Response, next: NextFunction) {
 		try {
-			const {startOfWeek} = req.body;
+			const {startOfWeek} = req.query;
+			if (typeof startOfWeek !== 'string')
+				return next(ApiError.BadRequest('Неверный запрос!'))
+
 			const day = await ScheduleBusinessService.getScheduleWeek(req.user, new Date(startOfWeek));
 			res.json(day.scheduleDays);
 		} catch (err) {
@@ -15,7 +19,10 @@ export class ScheduleController {
 
 	static async getScheduleWeekWithMarks(req: RequestWithUser, res: Response, next: NextFunction) {
 		try {
-			const {startOfWeek} = req.body;
+			const {startOfWeek} = req.query;
+			if (typeof startOfWeek !== 'string')
+				return next(ApiError.BadRequest('Неверный запрос!'))
+
 			const day = await ScheduleBusinessService.getScheduleWeekMarks(req.user, new Date(startOfWeek));
 			res.json(day);
 		} catch (err) {
