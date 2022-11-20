f1210e1ba72b71d6caa0b903f7a1e07eb76ab8b3
daniel <polundradanila@gmail.com>
2022-11-20 20:14:57 +0300

    feat(util): Добавление утилиты поиска начала недели

---
+++ client/src/utils/index.ts
@@ -0,0 +1,14 @@
+// @ts-ignore
+export function getMonday(d) {
+    d = new Date(d);
+    let day = d.getDay();
+    let diff = d.getDate() - day + (day == 0 ? -6 : 1);
+
+    let res = new Date(d.setDate(diff));
+    res.setHours(0);
+    res.setMinutes(0);
+    res.setSeconds(0);
+    res.setMilliseconds(0);
+    return res.toISOString()
+
+}
