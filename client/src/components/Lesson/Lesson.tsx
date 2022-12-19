f99247f12dbed716f83860cd7482e3b2c4253925
daniel <polundradanila@gmail.com>
2022-12-19 11:47:19 +0300

    style(component): Изменение стилей компонента

--- client/src/components/Lesson/Lesson.tsx
+++ client/src/components/Lesson/Lesson.tsx
@@ -1,6 +1,5 @@
 import {FC} from 'react';
-import {ListItemButton, ListItemText} from '@mui/material';
-import cl from '../learningDay/learningDay.module.css';
+import cl from './Lesson.module.css';
 import {Schedule} from "../../models/ISchedule";
 
 export interface LessonProps {
@@ -8,24 +7,22 @@ export interface LessonProps {
 }
 
 const Lesson: FC<LessonProps> = ({schedule}) => {
-    
+
     const teacherName = schedule.teacher.split(/\s+/).map((w, i) => i ? w.substring(0, 1).toUpperCase() + '.' : w).join(' ');
     const lessonsName = schedule.twoOurClassName.split('').length > 15 ? schedule.twoOurClassName.substring(0, 15) + '.' : schedule.twoOurClassName;
     const hours = (new Date(schedule.startTime).getHours().toString().length === 1) ? ('0' + new Date(schedule.startTime).getHours().toString()) : new Date(schedule.startTime).getHours().toString();
     const minutes = (new Date(schedule.startTime).getMinutes().toString().length === 1) ? '00' : new Date(schedule.startTime).getMinutes().toString();
     return (
-        <ListItemButton component="a" href="#simple-list">
-            <ListItemText
-                primary={lessonsName}
-                secondary={hours + ':' + minutes}
-                className={cl.textLesson}
-            />
-            <ListItemText
-                primary={'к. ' + schedule.audience}
-                secondary={teacherName}
-                className={cl.textLessonNotMark}
-            />
-        </ListItemButton>
+        <div className={cl.lessonContain}>
+            <div className={cl.lessonTextLeft}>
+                <p className={cl.lessonName}>{lessonsName}</p>
+                <p className={cl.teacherName}>{teacherName}</p>
+            </div>
+            <div className={cl.lessonTextRight}>
+                <p className={cl.timeOfLesson}>{hours}:{minutes}</p>
+                <p className={cl.numberOfCab}>к. {schedule.audience}</p>
+            </div>
+        </div>
     );
 };
 
--- client/src/components/Lesson/LessonMark.tsx
+++ client/src/components/Lesson/LessonMark.tsx
@@ -1,26 +1,23 @@
 import {FC} from 'react';
-import {ListItemButton, ListItemText} from '@mui/material';
-import cl from '../learningDay/learningDay.module.css';
+import cl from './Lesson.module.css';
 import {LessonProps} from "./Lesson";
 
 
 const LessonMark: FC<LessonProps> = ({schedule}) => {
     const mark = schedule.mark ? schedule.mark.toString() : 'y';
+    const teacherName = schedule.teacher.split(/\s+/).map((w, i) => i ? w.substring(0, 1).toUpperCase() + '.' : w).join(' ');
     const lessonsName = schedule.twoOurClassName.split('').length > 15 ? schedule.twoOurClassName.substring(0, 15) + '.' : schedule.twoOurClassName;
-    const hours = (new Date(schedule.startTime).getHours().toString().length === 1) ? ('0' + new Date(schedule.startTime).getHours().toString()) : new Date(schedule.startTime).getHours().toString();
-    const minutes = (new Date(schedule.startTime).getMinutes().toString().length === 1) ? '00' : new Date(schedule.startTime).getMinutes().toString();
     return (
-        <ListItemButton component="a" href="#simple-list">
-            <ListItemText
-                primary={lessonsName}
-                secondary={hours + ':' + minutes}
-                className={cl.textLesson}
-            />
-            <ListItemText
-                primary={mark}
-                className={cl.mark}
-            />
-        </ListItemButton>
+        <div className={cl.lessonContain}>
+            <div className={cl.lessonTextLeft}>
+                <p className={cl.lessonName}>{lessonsName}</p>
+                <p className={cl.teacherName}>{teacherName}</p>
+            </div>
+            <div className={cl.lessonMark}>
+                <p className={cl.markText}>{mark}</p>
+            </div>
+
+        </div>
     );
 };
 
