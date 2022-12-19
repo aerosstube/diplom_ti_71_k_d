b697036d84103a3dbdda8fa9705c79fadf9b44fb
daniel <polundradanila@gmail.com>
2022-12-19 11:45:45 +0300

    style(component): Изменение стилей компонента

--- client/src/components/learningDay/LearnigDayMark.tsx
+++ client/src/components/learningDay/LearnigDayMark.tsx
@@ -1,7 +1,8 @@
 import {FC} from 'react';
-import cl from './learningDay.module.css';
+import cl from './LearningDay.module.css';
 import {Schedule} from "../../models/ISchedule";
 import LessonMark from "../Lesson/LessonMark";
+
 
 export interface LearningDayProps {
     nameOfDay: string;
@@ -12,11 +13,11 @@ export interface LearningDayProps {
 const LearningDayMark: FC<LearningDayProps> = ({nameOfDay, schedules}) => {
     return (
         <div className={cl.dayPlace}>
-            <p className={cl.textDay}>{nameOfDay ? nameOfDay : 'Суббота'}</p>
-            <div className={cl.learningDay}>
+            <p className={cl.dayName}>{nameOfDay ? nameOfDay : 'Суббота'}</p>
+            <div className={cl.dayContain}>
                 {
                     schedules.length !== 0 ? schedules.map((lesson) => <LessonMark schedule={lesson}/>) :
-                        <h1 className={cl.textDay} style={{textAlign: 'center', marginTop: '145px'}}> Нет занятий</h1>
+                        <h1 className={cl.dayTextNonLessons}>Нет занятий</h1>
                 }
             </div>
         </div>
