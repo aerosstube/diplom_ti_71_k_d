09354d0c323976c1d76f617d2377345011600bc5
daniel <polundradanila@gmail.com>
2022-12-26 21:55:00 +0300

    feat(service): Добавление запросов к API

--- client/src/services/TeacherService.ts
+++ client/src/services/TeacherService.ts
@@ -1,12 +1,16 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/dist/query/react";
 import {baseURL} from "./config";
 import {RootState} from "../store/store";
+import {IGroups} from "../models/IGroups";
+import {ILessons} from "../models/ILessons";
+import {IBDMarks} from "../models/IBDMarks";
+import {IMarksToBD} from "../models/IMarksToBD";
 
 
 export const teacherAPI = createApi({
     reducerPath: 'teacherAPI',
     baseQuery: fetchBaseQuery({
-        baseUrl: `${baseURL}/api/schedule`,
+        baseUrl: `${baseURL}/api/teacher`,
         prepareHeaders: (headers, {getState}) => {
             const token = (getState() as RootState).userReducer.tokens?.tokens.accessToken;
             if (token) {
@@ -16,15 +20,36 @@ export const teacherAPI = createApi({
         }
     }),
     endpoints: (build) => ({
-        fetchGroups: build.query<string[], string>({
-            query: (startOfWeek) => ({
-                url: '/get_week_marks',
+        fetchGroups: build.query<IGroups[], string>({
+            query: () => ({
+                url: '/getAllowedGroups',
+                method: 'GET',
+            }),
+        }),
+        fetchClasses: build.query<ILessons, string>({
+            query: () => ({
+                url: '/getAllowedClasses',
+                method: 'GET',
+            })
+        }),
+        fetchMarks: build.query<IBDMarks, string>({
+            query: (ids: string) => ({
+                url: '/getStudentsMarks',
                 method: 'GET',
                 params: {
-                    startOfWeek
+                    classId: ids.split('')[1],
+                    groupId: ids.split('')[0],
                 }
+            })
+        }),
+        updateMarks: build.mutation<any, IMarksToBD>({
+            query: (obj: IMarksToBD) => ({
+                url: '/updateStudentMark',
+                method: 'POST',
+                body: obj
 
-            }),
+
+            })
         })
     })
 });
